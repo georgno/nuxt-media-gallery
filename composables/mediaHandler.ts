14d499da919aac07f4972224a3c8dabf656b2853
georg <noerig@protonmail.com>
2024-12-20 21:28:48 +0100

    [wip] added mediahandler

--- composables/mediaHandler.ts
+++ composables/mediaHandler.ts
@@ -1,13 +1,67 @@
-export const mediaHandler = () => {
-    return getDummyData();
+import { createStorage } from "unstorage";
+import indexedDbDriver from "unstorage/drivers/indexedb";
+
+interface Media {
+    id: number;
+    title: string;
+    alt: string;
+    path: string;
 }
 
-function getDummyData() {
-    return [
-        { id: 1, title: 'media 1', alt: 'foo' },
-        { id: 2, title: 'media 2', alt: 'bar' },
-        { id: 3, title: 'media 3', alt: 'baz' },
-        { id: 4, title: 'media 4', alt: 'qux' },
-        { id: 5, title: 'media 5', alt: 'quux' },
-    ]
-}
+export const mediaHandler = () => {
+    const storage = createStorage({
+        driver: indexedDbDriver({ base: "media:" }),
+    });
+
+    const create = async (data: Media) => {
+        const key = await getNextKey();
+        await storage.setItem(key.toString(), data);
+    };
+
+    const deleteItem = async (key: string) => {
+        console.log('delete', key);
+        await storage.remove(key.toString());
+    }
+
+    const getMedia = async (key: string): Promise<Media | null> => {
+        const fullKey = key.startsWith('media:') ? key : `media:${key}`;
+        return await storage.getItem(fullKey);
+    };
+
+    const getAllMedias = async (): Promise<Media[]> => {
+        const keys = await storage.getKeys();
+        console.log(keys);
+        const medias: Media[] = [];
+        for (let key of keys) {
+            key = key.replace('media:', '');
+
+            const media = await storage.getItem<Media>(key);
+            console.log(media, key);
+            if (media) {
+                medias.push(media);
+            }
+        }
+
+        console.log(medias);
+        return medias;
+    };
+
+    const getMediaCount = async (): Promise<number> => {
+        const keys = await storage.getKeys();
+        return keys.length;
+    };
+
+    const getNextKey = async (): Promise<number> => {
+        const count = await storage.getKeys();
+        return count.length + 1;
+    };
+
+    return {
+        create,
+        deleteItem,
+        getMedia,
+        getAllMedias,
+        getMediaCount,
+        getNextKey,
+    };
+};
