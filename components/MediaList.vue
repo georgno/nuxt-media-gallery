030b9a548a742a7751657ff943c850bd5cf7735a
georg <noerig@protonmail.com>
2025-01-16 00:02:39 +0100

    added flavour

--- components/MediaList.vue
+++ components/MediaList.vue
@@ -15,9 +15,9 @@ const medias = computed(() => {
 <template>
   <div :class="medias.length ? 'grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3' : 'flex items-center justify-center h-full'">    <MediaCard v-for="item in medias" :key="item.id" :media="item" />
 
-    <div v-if="!medias.length" class="text-center font-bold text-gray-300 dark:text-gray-500" style="font-size: 1.2rem">
-      No media yet. <br>
-      Maybe you should add some? :-)
+    <div v-if="!medias.length" class="text-center font-bold text-gray-300 dark:text-gray-500" style="font-size: 1.2rem;">
+      <p>Nothing to see yet. <br>
+        Maybe you should add something?</p>
     </div>
   </div>
 </template>
